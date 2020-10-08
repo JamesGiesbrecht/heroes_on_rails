41833f66462dfaaf4ef5f95eb84e3fc138588210
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-10-07 19:53:57 -0500

    Defining the one-to-many relationships

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,2 +1,4 @@
 class Hero < ApplicationRecord
+  belongs_to :publisher
+  belongs_to :race
 end
--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,2 +1,4 @@
 class Publisher < ApplicationRecord
+  # If a publisher is destroyed, associated heroes are as well
+  has_many :heroes, dependant: destroy
 end
