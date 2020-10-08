008f3c640d6a3422333dcf91e82219c8ed677742
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-10-07 21:32:48 -0500

    Making belongs_to associations optional

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,5 +1,5 @@
 class Hero < ApplicationRecord
-  belongs_to :publisher
-  belongs_to :race
+  belongs_to :publisher, optional: true
+  belongs_to :race, optional: true
   has_and_belongs_to_many :powers
 end
