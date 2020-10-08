bea9ed81f03786aaccd62a9bfdc7d33ef2428db8
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-10-07 19:57:33 -0500

    Defining many to many relationship

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,4 +1,5 @@
 class Hero < ApplicationRecord
   belongs_to :publisher
   belongs_to :race
+  has_and_belongs_to_many :powers
 end
---
+++ db/migrate/20201008005507_hero_powers.rb
@@ -0,0 +1,4 @@
+class HeroPowers < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
