a9b3335636e3539036f473e8326e33fc6004222e
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-10-15 11:00:05 -0500

    Importing and parsing JSON

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+
+Hero.destroy_all
+Power.destroy_all
+Publisher.destroy_all
+Race.destroy_all
+
+heroes_data = File.read(Rails.root.join('db/data_sources/normalized/heroes.json'))
+
+heroes = JSON.parse(heroes_data)
+
+heroes.each do |hero|
+  puts hero['name']
+end
