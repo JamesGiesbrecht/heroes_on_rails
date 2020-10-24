84e391c2f4a12f84cac49296dbe486ff232f8a95
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-10-23 19:12:11 -0500

    Generating race and publisher controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@
 Rails.application.routes.draw do
   root 'application#index'
   resources :heroes, only: %i[show index]
+  resources :publisher, only: %i[show index]
+  resources :race, only: %i[show index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
