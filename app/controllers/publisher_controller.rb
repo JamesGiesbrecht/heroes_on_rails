6f0e0a1af021041b9264f1b69377e80cd7c5c228
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-10-23 19:29:58 -0500

    Making index views for publishers and races

--- app/controllers/publisher_controller.rb
+++ app/controllers/publisher_controller.rb
@@ -1,7 +1,9 @@
 class PublisherController < ApplicationController
   def index
+    @publishers = Publisher.all
   end
 
   def show
+    @publisher = Publisher.find(params[:id])
   end
 end
--- app/controllers/race_controller.rb
+++ app/controllers/race_controller.rb
@@ -1,7 +1,9 @@
 class RaceController < ApplicationController
   def index
+    @races = Race.all
   end
 
   def show
+    @race = Race.find(params[:id])
   end
 end
